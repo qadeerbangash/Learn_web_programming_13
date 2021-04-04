95b0a67a93814224f0871dc3924d33ccd27af15d
Qadeer Hussain <qadeeerhussain494@gmail.com>
2021-04-04 12:08:01 +0500

    Just Practice

---
+++ app.js
@@ -0,0 +1,49 @@
+// for(var i=1;i<=100;i=i+10){
+//    for(var j=i;j<i+10;j++){
+//     document.write(j+" ")
+//     console.log(j)
+//    }
+//    document.write("<br>")
+// }
+
+// var n="qadeer hussain is Name"
+// var p=n.replace("is Name","is my Name")
+// console.log(p);
+
+// var n="My Name is Qadeer Khan,My Name is Qadeer Khan,My Name is Qadeer Khan "
+// var p=n.replace(/Khan/g,"Hussain")
+// console.log(p);
+
+// var n=2.5;
+// var num=Math.round(n)
+// alert(num)
+
+// var n=1.0;
+// var p=Math.ceil(n)
+// alert(p)
+
+
+// var n=4.9;
+// var p=Math.floor(n)
+// alert(p)
+
+// var b=Math.random()*10;
+// console.log(b)
+
+// var hu=prompt("Enter head user Name")
+// var tu=prompt("Enter tail user Name")
+// var toss=Math.random()*2;
+// var floor=Math.floor(toss)
+// if(floor===0){
+//     alert("Heads"+" "+hu+" "+"win")
+// }else{
+//     alert("Tail"+" "+tu+" "+"win")
+// }
+
+
+// var a=Number("22")
+// console.log(a)
+
+  var a=11.765532;
+  var b=a.toFixed(3);
+  alert(b)
